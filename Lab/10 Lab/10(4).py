2001080bd1d785e9c51f196929fd940ebdbbc769
Aviamore <96797655+Aviamore@users.noreply.github.com>
2022-12-01 23:13:40 +0300

    Update 10(4).py

--- Lab/10 Lab/10(4).py
+++ Lab/10 Lab/10(4).py
@@ -1,9 +1,17 @@
-a = [int(s) for s in input().split()]
-b = list(set(a))
-for i in range (len(a)):
-    if (a[i] == b[i]):
-        print ("NO")
-    else:
-        print ("YES")
-print (a)
-print (b)
+from random import randint
+
+
+def random():
+    lst = [randint(1, 10) for i in range(5)]
+    print(lst)
+    return lst
+def var4():
+    lst = random()
+    s_lst = set(lst)
+    for i in s_lst:
+        if lst.count(i) > 1:
+            print(f"{i} YES")
+        else:
+            print(f"{i} NO")
+
+var4()
