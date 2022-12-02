90257105b39d265b1738576636d87e43d66531a7
Aviamore <96797655+Aviamore@users.noreply.github.com>
2022-12-02 10:13:38 +0300

    Update Lab10(8).py

--- Lab/10 Lab/Lab10(8).py
+++ Lab/10 Lab/Lab10(8).py
@@ -1,11 +1,13 @@
 b = int(input())
 a = [int(s) for s in input().split()]
-if (len(a)/2 == (len(a)%2 == 0)):
+k = len(a)
+if ((b/2) and ((len(a) % 2) == 1)):
     print ("NO")
 else:
     for i in range (len(a)):
         if (b == a[i]):
             print ("YES")
         else:
-            print ("NO")
-            break
+            k = k - 1
+            if (k == 0):
+                print ("NO")
